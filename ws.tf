a50054a44e6a7290f6727bf62e8a9d4fbdddd05a
Viktor Didenchuk <Perepyolka@users.noreply.github.com>
2020-05-27 11:52:02 +0300

    Update ws.tf

--- ws.tf
+++ ws.tf
@@ -1,10 +1,10 @@
 resource "scalr_workspace" "ws" {
   name = "test3"
   organization = "org-sgpvfvrkj5ao2j0"
+  working_directory = ""
   
   vcs_repo {
     identifier = "perepyolka/terraform-scalr-eapeape"
     oauth_token_id = "vcs-stqieqfflva0gfg"
-    path = ""
   }
 }
